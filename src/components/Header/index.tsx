c672503028501937d6fdd8f222afd71b7e2f4ded
jander.nunes <jander.nunes@conechtus.org.br>
2022-03-14 13:19:14 -0400

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
 
 export function Header() {
   return(
-    <header>
-      <img src={logoImg} alt="Jl Money" />
-      <button type="button">
-        Nova Transação
-      </button>
-    </header>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Jl Money" />
+        <button type="button">
+          Nova Transação
+        </button>
+      </Content>
+    </Container>
   )
 }
