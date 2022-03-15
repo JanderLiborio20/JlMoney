e2fc2b0a08e085370400693255321bd826e3c2a1
jander.nunes <jander.nunes@conechtus.org.br>
2022-03-15 17:52:19 -0400

    modal completo sem o 2btn do meio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import logoImg from '../../assets/logo.svg';
+
 import { Container, Content } from './styles';
 
-export function Header() {
+
+interface handleModalOpen {
+  onOpenNewTransactionModal: () => void;
+}
+export function Header({ onOpenNewTransactionModal }: handleModalOpen) {
+
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="Jl Money" />
-        <button type="button">
+        <button 
+        type="button" 
+        onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
+
+
       </Content>
     </Container>
   )
